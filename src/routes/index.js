fdabc1d93fb2eddc6a5e114f3b3cb92da2d5dd4c
dimasdevspro <dimasdevspro@github.com>
2021-01-29 14:55:45 -0300

    first commit

---
+++ src/routes/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const routes = express.Router()
+const HomeController = require('../app/controllers/HomeController')
+
+const products = require('./products')
+const users = require('./users')
+
+routes.get("/", HomeController.index);
+
+routes.use('/products', products)
+routes.use('/users', users)
+
+//Views
+routes.get("/views/about", function(req, res){
+    return res.render("about.njk")
+})
+
+// Alias
+routes.get("/ads/create", function(req, res){
+    return res.redirect("/products/create")
+});
+
+routes.get('/accounts', function(req, res){
+    return res.redirect("/users/registers")
+})
+
+module.exports = routes
---
+++ src/routes/users.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const routes = express.Router()
+
+const SessionController = require('../app/controllers/SessionController')
+const UserController = require('../app/controllers/UserController')
+
+const Validator = require('../app/validators/user')
+// // login/logout
+// routes.get('/login', Sessioncontroller.loginForm)
+// routes.post('/login', Sessioncontroller.login)
+// routes.post('/logout', Sessioncontroller.logout)
+
+// // reset password / forgot
+// routes.get('/forgot-passowrd', Sessioncontroller.forgotForm)
+// routes.get('/password-reset', Sessioncontroller.resetForm)
+// routes.post('/forgot-passowrd', Sessioncontroller.forgot)
+// routes.post('/password-reset', Sessioncontroller.reset)
+
+// // user register
+
+routes.get('/registers', UserController.registerForm)
+routes.post('/register', Validator.post, UserController.post)
+
+// routes.post('/', UserController.show)
+// routes.put('/', UserController.update)
+// routes.delete('/', UserController.delete)
+
+
+module.exports = routes
